c25bc7e531e96a50f5755f416707d5886021930c
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2024-06-24 16:05:58 +0200

    Fix remove hook (#48)
    * Improve install/remove/setup logs and comments
    
    * Fix directory removal in remove hook
    
    * Allow firewall setup from remove hook

--- snap/local/stage/bin/otbr-setup.sh
+++ snap/local/stage/bin/otbr-setup.sh
@@ -30,6 +30,8 @@
 
 echo "OTBR oneshot setup service"
 
+# Create directory for the OpenThread Posix Config Domain Socket
+# This path is set with a build flag when building OTBR
 mkdir -p /run/snap.$SNAP_NAME
 
 # The operations performed here get reversed upon reboot and removal of the snap.
